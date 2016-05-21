dfe53f1a0cbc63761b7a7b16888d2921ba2b8cb7
Jahangir Zinedine <jzinedine@gmail.com>
2016-05-21 16:28:05 +0430

    send messages to telegram, still needs tagging with date and apartment size

--- Gemfile
+++ Gemfile
@@ -4,3 +4,6 @@ gem 'thor'
 gem 'selenium-webdriver'
 gem 'rspec'
 gem 'sqlite3'
+gem 'telegram-bot-ruby'
+gem 'pry'
+
---
+++ lib/telegram_bot.rb
@@ -0,0 +1,13 @@
+class TelegramBot
+  def initialize
+    @token = ENV['telegram_bot_token']
+    Telegram::Bot::Client.run(@token) do |bot|
+      results = $db.execute('select * from ads')
+      results.each do |ad|
+        bot.api.send_message(chat_id: '@hamshahri_ads', text: ad[2])
+      end
+    end
+  end
+end
+
+TelegramBot.new
