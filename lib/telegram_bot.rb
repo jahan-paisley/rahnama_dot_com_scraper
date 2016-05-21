5f75e1d5826d2b7bbc37c4d5f34a1e58687a0d78
Jahangir Zinedine <jzinedine@gmail.com>
2016-05-21 20:47:51 +0430

    added more info to telegram messages including their category as tag and contact phone

--- lib/telegram_bot.rb
+++ lib/telegram_bot.rb
@@ -4,8 +4,21 @@ class TelegramBot
     Telegram::Bot::Client.run(@token) do |bot|
       results = $db.execute('select * from ads')
       results.each do |ad|
-        bot.api.send_message(chat_id: '@hamshahri_ads', text: ad[2])
+        message = build_message(ad)
+        bot.api.send_message(chat_id: '@hamshahri_ads', text: message)
+        sleep(5)
       end
     end
+  end
+
+  def build_message(ad)
+    phones = $db.execute('select * from phones')
+    ad_text = ad[2].gsub(/\d{8,11}/, '')
+    <<-MSG
+#{ad_text}
+##{ad[6].gsub('-', '_')}
+#{phones.select { |e| e[1] == ad[1] }.map { |e| e[0].length==8 ? "021"+e[0] : e[0] }.join(" ")}
+    MSG
+
   end
 end
