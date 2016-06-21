f637ac0bf8a7be9d7f4d559b76c509b08599b70e
Jahangir Zinedine <jzinedine@gmail.com>
2016-06-21 23:09:59 +0430

    switched to chrome on devices with firefox 47 which doesn't work with selenium

--- lib/capybara_config.rb
+++ lib/capybara_config.rb
@@ -8,16 +8,16 @@ class CapybaraConfig
     Capybara.default_max_wait_time = 15
 
     Capybara.register_driver :selenium_firefox_nojs do |app|
-      profile = Selenium::WebDriver::Firefox::Profile.new
-      profile['javascript.enabled'] = false
-      profile['permissions.default.image'] = 2
-      if proxy
-        profile["network.proxy.type"] = 1
-        profile["network.proxy.socks"] = proxy.split(':')[0]
-        profile["network.proxy.socks_port"] = proxy.split(':')[1].to_i
-        profile["network.proxy.socks_remote_dns"] = true
-      end
-      Capybara::Selenium::Driver.new(app, :browser => :firefox, :profile => profile)
+      # profile = Selenium::WebDriver::Chrome::Profile.new
+      # profile['javascript.enabled'] = false
+      # profile['permissions.default.image'] = 2
+      # if proxy
+      #   profile["network.proxy.type"] = 1
+      #   profile["network.proxy.socks"] = proxy.split(':')[0]
+      #   profile["network.proxy.socks_port"] = proxy.split(':')[1].to_i
+      #   profile["network.proxy.socks_remote_dns"] = true
+      # end
+      Capybara::Selenium::Driver.new(app, :browser => :chrome)
     end
     Capybara.default_driver = :selenium_firefox_nojs
   end
