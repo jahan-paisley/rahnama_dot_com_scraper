3c726da838073e0e7370cb47156b04942e25fb2e
Jahnagir Zinedine <jzinedine@gmail.com>
2016-10-13 02:09:57 +0330

    added aread to ES completed

--- lib/area_code_helper.rb
+++ lib/area_code_helper.rb
@@ -1,14 +1,19 @@
 require 'json'
 
-module AreadCodeHelper
-
-  @@aread_codes= JSON.parse(IO.read(File.expand_path("../../data/area_codes.json", __FILE__)))
-
-  def find_aread_code phone
-    begin
-       area = aread_codes.values.flatten.select { |e| e.values.flatten.include?(phone[0...4].to_i) }.first.keys.first
-    rescue
-    end if phone
-    return area
+module AreaCodeHelper
+  def self.included(base)
+    base.extend ClassMethods
   end
+
+  module ClassMethods
+    def find_area_code phone
+      $area_codes= $area_codes || JSON.parse(IO.read(File.expand_path("../../data/area_codes.json", __FILE__)))
+      begin
+        area = $area_codes.values.flatten.select { |e| e.values.flatten.include?(phone[0...4].to_i) }.first.keys.first
+      rescue
+      end if phone
+      return area
+    end
+  end
+
 end
--- lib/elasticsearch_client.rb
+++ lib/elasticsearch_client.rb
@@ -6,12 +6,11 @@ require './lib/area_code_helper'
 $elasticsearch_client = Elasticsearch::Client.new()
 
 class ElasticsearchClient
-  include AreadCodeHelper
+  include AreaCodeHelper
 
   def self.import_ads
-    columns, *rows = $db.execute2("select a.*, no from ads a left outer join phones ph on a.person_id=ph.person_id where id > ?", (IO.read("data/.last_exported_id") || '0').to_i)
+    columns, *rows = $db.execute2("select * from ads where id > ?", (IO.read("data/.last_exported_id") || '0').to_i)
     prows= rows.map { |e| Hash[columns.zip e] }
-    # (1..13000).each{|e| begin client.delete(index: 'ads', type:'ads', id: e) rescue puts ' '; end}
     prows.each do |row|
       $elasticsearch_client.index(index: 'ads', type: 'ads', id: row["id"], body: build_ui_friendly_hash(row));
     end;
@@ -30,7 +29,8 @@ class ElasticsearchClient
     end.flatten
     added_pdate = Hash[*converted_map]
     pdate = JalaliDate.new(Date.parse(row['date'])).strftime("%Y%n%d").to_i
-    added_pdate.merge({pdate: pdate, area: find_area_code(row['no'])})
+    phone= row['ad_text'].scan(/(?<!\d)[2-9][0-9]{7}/)[0] rescue nil
+    added_pdate.merge({pdate: pdate, area: ElasticsearchClient.find_area_code(phone)})
   end
 
 end
